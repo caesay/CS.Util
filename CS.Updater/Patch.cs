8472ef093a45fd546f4663532ca2aca9b4a4d5cc
Caelan Sayler <caelantsayler@gmail.com>
2016-04-07 19:03:32 -0600

    Initially playing with some updater stuff.

---
+++ CS.Updater/Patch.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CS.Util.Extensions;
+
+namespace CS.Updater
+{
+    public abstract class UpdateTask
+    {
+        public abstract Task PrepareInstall(UpdateContext context);
+        public abstract Task ExecuteInstall(UpdateContext context);
+
+        public abstract Task PrepareUninstall();
+        public abstract Task ExecuteUninstall();
+
+        public abstract Task Rollback();
+        public abstract Task Uninstall();
+    }
+
+    public class UpdateContext
+    {
+        public Version Current { get; }
+        public bool ColdRun { get; }
+        public string Directory { get; }
+    }
+}
