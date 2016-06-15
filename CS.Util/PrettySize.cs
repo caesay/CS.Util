4d5c306b9aacec4b3ac33d76058bbb75a98a5a3d
Caelan Sayler <caelantsayler@gmail.com>
2016-06-14 18:55:37 -0600

    Add PrettySize, include missing Generic.xaml

---
+++ CS.Util/PrettySize.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS.Util
+{
+    public static class PrettySize
+    {
+        public static string Format(long bytes, int decimalPlaces = 2)
+        {
+            if (bytes < 1000) return bytes + " B";
+            if (bytes < 1000000) return Math.Round(bytes / (double)1000, decimalPlaces) + " KB";
+            if (bytes < 1000000000) return Math.Round(bytes / (double)1000000, decimalPlaces) + " MB";
+            return Math.Round(bytes / (double)1000000000, decimalPlaces) + " GB";
+        }
+    }
+}
